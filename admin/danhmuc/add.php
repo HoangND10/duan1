b7869c1f2affc25eafbd2d05077362e8cdae725e
HoangND10 <ichigo12012001@gmail.com>
2024-03-18 21:31:04 +0700

    qly sp & dm

--- admin/danhmuc/add.php
+++ admin/danhmuc/add.php
@@ -1,22 +1,26 @@
-<div class="row2">
-         <div class="row2 font_title">
-          <h1>Thêm danh mục</h1>
-         </div>
-         <div class="row2 form_content ">
-          <form action="#" method="POST">
-           <div class="row2 mb10">
-            <label>Tên Danh Mục </label> <br>
-            <input type="text" name="tendanhmuc" placeholder="nhập vào tên">
-           </div>
-           <div class="row mb10 ">
-         <input class="mr20" type="submit" value="THÊM MỚI"name="themmoi">
-         <a href="index.php?act=listdm"><input  class="mr20" type="button" value="DANH SÁCH"></a>
-           </div>
-          </form>
-         </div>
-        </div>
-
-        <?php
-            if(isset($thongbao)&&($thongbao)!="") echo "$thongbao";
-
-        ?>
+<div class="row">
+  <div class="row frmtitle">
+    <H1> THÊM MỚI LOẠI HÀNG HÓA</H1>
+  </div>
+  <div class="row frmcontent">
+    <form action="index.php?act=adddm" method="post">
+      <div class="row mb10">
+        Mã loại<br>
+        <input type="text" name="maloai" disabled>
+      </div>
+      <div class="row mb10">
+        Tên loại<br>
+        <input type="text" name="tenloai">
+      </div>
+      <div class="row mb10">
+        <input type="submit" name="themmoi" value="THÊM MỚI">
+        <input type="reset" value="NHẬP LẠI">
+        <a href="index.php?act=listdm"><input type="button" value="DANH SÁCH"></a>
+      </div>
+      <?php
+      if (isset($thongbao) && ($thongbao != "")) echo $thongbao;
+      ?>
+    </form>
+  </div>
+</div>
+</div>
--- admin/danhmuc/list.php
+++ admin/danhmuc/list.php
@@ -1,57 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-        
-</body>
-</html>
-
-
-<div class="row2">        
-         <div class="row2 font_title">
-          <h1>DANH SÁCH DANH MỤC</h1>
-         </div>
-         <div class="row2 form_content ">
-          <form action="#" method="POST">
-           <div class="row2 mb10 formds_loai">
-           <table>
-            <tr>
-                <th></th>
-                <th>ID</th>
-                <th>TÊN DANH MỤC</th>
-                <th></th>
-            </tr>
-            <?php
-                foreach($listdm as $dm){
-                    extract($dm);
-                    $xoadm="index.php?act=xoadm&&iddm=".$iddm;
-                    $suadm="index.php?act=suadm&&iddm=".$iddm;
-                    echo '   <tr>
-                    <td><input type="checkbox" name="" id=""></td>
-                    <td>'.$iddm.'</td>
-                    <td>'.$tendm.'</td>
-                    <td><a href='.$suadm.'><input type="button" value="Sửa"></a>
-                        <a href='.$xoadm.'><input type="button" value="Xóa"></a>
-                    </td>
-                    </tr>';
-                }
-            ?>    
-              
-            
-           </table>
-           </div>
-           <div class="row mb10 ">
-                <a href="index.php?act=adddm"> <input  class="mr20" type="button" value="NHẬP THÊM"></a>
-           </div>
-          </form>
-         </div>
-        </div>
-
-
-
-       
+<div class="row">
+    <div class="row frmtitle">
+        <h1>DANH SÁCH LOẠI HÀNG HÓA</h1>
     </div>
+    <div class="row frmcontent">
+        <div class="row mb10 frmdsloai">
+            <table>
+                <tr>
+                    <th></th>
+                    <th>MÃ LOẠI</th>
+                    <th>TÊN LOẠI</th>
+                    <th></th>
+                </tr>
+                <?php
+                foreach ($listdanhmuc as $danhmuc) {
+                    extract($danhmuc);
+                    $suadm = "index.php?act=suadm&id=" . $iddm;
+                    $xoadm = "index.php?act=xoadm&id=" . $iddm;
+                    echo '<tr>
+                            <td><input type="checkbox" name="" id=""></td>
+                            <td>' . $iddm . '</td>
+                            <td>' . $tendm . '</td>
+                            <td><a href="' . $suadm . '"><input type="button" value="Sửa"></a><a href="' . $xoadm . '"><input type="button" value="Xóa"></a></td>
+                        </tr>';
+                }
+                ?>
+
+            </table>
+        </div>
+        <div class="row mb10">
+            <input type="button" value="CHỌN TẤT CẢ">
+            <input type="button" value="BỎ CHỌN TẤT CẢ">
+            <input type="button" value="XÓA CÁC MỤC ĐÃ CHỌN">
+            <a href="index.php?act=adddm">
+                <input type="button" value="NHẬP THÊM"></a>
+        </div>
+    </div>
+</div>
