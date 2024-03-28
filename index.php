1cd3724f3a65319fb487102b11acba2c31838ce0
HoangND10 <ichigo12012001@gmail.com>
2024-03-28 21:42:16 +0700

    gio hang

--- index.php
+++ index.php
@@ -5,12 +5,14 @@ ini_set('display_errors', 1);
 
 ob_start();
 session_start();
+include "global.php";
 include "model/taikhoan.php";
 include 'model/pdo.php';
 include 'model/danhmuc.php';
 include 'model/sanpham.php';
+include 'model/donhang.php';
 include 'views/header.php';
-
+if (!isset($_SESSION['mycart'])) $_SESSION['mycart'] = [];
 $sanpham = loadall_sanpham_home();
 
 $listdanhmuc = loadall_danhmuc();
@@ -18,29 +20,29 @@ $listdanhmuc = loadall_danhmuc();
 if ((isset($_GET['act'])) && ($_GET['act'] != "")) {
     $act = $_GET['act'];
     switch ($act) {
-            case "sanpham":
-                if(isset($_POST['kyw'])&&($_POST['kyw']!="")){
-                    $kyw=$_POST['kyw'];
-                }else{
-                    $kyw="";
-                }
-                if(isset($_GET['iddm'])&&($_GET['iddm']>0)){
-                    $iddm=$_GET['iddm'];
-                }else{
-                    $iddm = 0;
-                }
-                    $listsanpham=loadall_sanpham($kyw,$iddm);
-                    include "views/sanpham.php";
-                    break;
+        case "sanpham":
+            if (isset($_POST['kyw']) && ($_POST['kyw'] != "")) {
+                $kyw = $_POST['kyw'];
+            } else {
+                $kyw = "";
+            }
+            if (isset($_GET['iddm']) && ($_GET['iddm'] > 0)) {
+                $iddm = $_GET['iddm'];
+            } else {
+                $iddm = 0;
+            }
+            $listsanpham = loadall_sanpham($kyw, $iddm);
+            include "views/sanpham.php";
             break;
-            case "chitietsp":
-                if(isset($_GET['idsp'])&&($_GET['idsp']>0)){
-                    $sanpham=loadone_sanpham($_GET['idsp']);
-                }
-                $spcungloai=load_sanpham_cungloai($_GET['idsp'],$sanpham['iddm']);
-                include "views/chitietsp.php";
-                break;
-        
+            break;
+        case "chitietsp":
+            if (isset($_GET['idsp']) && ($_GET['idsp'] > 0)) {
+                $sanpham = loadone_sanpham($_GET['idsp']);
+            }
+            $spcungloai = load_sanpham_cungloai($_GET['idsp'], $sanpham['iddm']);
+            include "views/chitietsp.php";
+            break;
+
 
         case 'shop':
             include "views/shop.php";
@@ -104,21 +106,60 @@ if ((isset($_GET['act'])) && ($_GET['act'] != "")) {
             include "index.php";
             break;
 
-            case "about":
-                include "views/trangcon/about.php";
-                break;
-            case "tintuc":
-                include "views/trangcon/tintuc.php";
-                break;
-            case "lienhe":
-                include "views/trangcon/lienhe.php";
-                break;
-            case "giohang":
-                include "views/viewcart/giohang.php";
+        case "about":
+            include "views/trangcon/about.php";
             break;
-
-    
-    
+        case "tintuc":
+            include "views/trangcon/tintuc.php";
+            break;
+        case "lienhe":
+            include "views/trangcon/lienhe.php";
+            break;
+        case "bill":
+            include "views/viewcart/mybill.php";
+            break;
+        case "addtocart":
+            if (isset($_POST['addtocart']) && ($_POST['addtocart'])) {
+                $idsp = $_POST['idsp'];
+                $tensp = $_POST['tensp'];
+                $hinh = $_POST['hinh'];
+                $gia = $_POST['gia'];
+                $soluong = 1;
+                $ttien = $soluong * $gia;
+                $spadd = [$idsp, $tensp, $hinh, $gia, $soluong, $ttien];
+                array_push($_SESSION['mycart'], $spadd);
+            }
+            include "views/home.php";
+            break;
+        case "deletecart":
+            if (isset($_GET['idcart'])) {
+                $idcart = $_GET['idcart'];
+                unset($_SESSION['mycart'][$idcart]);
+                $_SESSION['mycart'] = array_values($_SESSION['mycart']);
+            }
+            header("location: index.php");
+            exit();
+            break;
+        case "donhang":
+            if (isset($_POST['thanhtoan']) && ($_POST['thanhtoan'])) {
+                if (isset($_SESSION['taikhoan']['iduser'])) {
+                    $iduser = $_SESSION['taikhoan']['iduser'];
+                    $tongtien = tt();
+                    $pttt = "Tiền mặt";
+                    $trangthai = "1";
+                    $order_date = date('Y-m-d H:i:s');
+                    // echo "<pre>";
+                    // print_r([$iduser, $tongtien, $pttt, $order_date, $trangthai]);
+                    // echo "</pre>";
+                    insert_donhang($iduser, $tongtien, $pttt, $order_date, $trangthai);
+                    $thongbao = "Đặt hàng thành công";
+                    // header("Location: " . $_SERVER['HTTP_REFERER']);
+                } else {
+                    header("Location: index.php?act=dangnhap");
+                }
+            }
+            include "views/viewcart/mybill.php";
+            break;
     }
 } else {
     include 'views/home.php';
--- views/viewcart/mybill.php
+++ views/viewcart/mybill.php
@@ -1,34 +1,34 @@
 <div class="offcanvas-overlay"></div>
-    <!-- breadcrumb-area start -->
-    <div class="breadcrumb-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="row breadcrumb_box  align-items-center">
-                        <div class="col-lg-6 col-md-6 col-sm-12 text-center text-md-start">
-                            <h2 class="breadcrumb-title">Shop</h2>
-                        </div>
-                        <div class="col-lg-6  col-md-6 col-sm-12">
-                            <!-- breadcrumb-list start -->
-                            <ul class="breadcrumb-list text-center text-md-end">
-                                <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-                                <li class="breadcrumb-item active">Checkout</li>
-                            </ul>
-                            <!-- breadcrumb-list end -->
-                        </div>
+<!-- breadcrumb-area start -->
+<div class="breadcrumb-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="row breadcrumb_box  align-items-center">
+                    <div class="col-lg-6 col-md-6 col-sm-12 text-center text-md-start">
+                        <h2 class="breadcrumb-title">Shop</h2>
+                    </div>
+                    <div class="col-lg-6  col-md-6 col-sm-12">
+                        <!-- breadcrumb-list start -->
+                        <ul class="breadcrumb-list text-center text-md-end">
+                            <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                            <li class="breadcrumb-item active">Checkout</li>
+                        </ul>
+                        <!-- breadcrumb-list end -->
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
 
-    <!-- breadcrumb-area end -->
+<!-- breadcrumb-area end -->
 
-    <!-- checkout area start -->
-    <div class="checkout-area pt-100px pb-100px">
-        <div class="container">
+<!-- checkout area start -->
+<div class="checkout-area pt-100px pb-100px">
+    <div class="container">
+        <form action="index.php?act=donhang" method="post">
             <div class="row">
-                
                 <div class="col-lg-5 mt-md-30px mt-lm-30px ">
                     <div class="your-order-area">
                         <h3>Your order</h3>
@@ -40,12 +40,24 @@
                                         <li>Total</li>
                                     </ul>
                                 </div>
-                                <div class="your-order-middle">
-                                    <ul>
-                                        <li><span class="order-middle-left">Product Name X 1</span> <span class="order-price">$329 </span></li>
-                                        <li><span class="order-middle-left">Product Name X 1</span> <span class="order-price">$329 </span></li>
-                                    </ul>
-                                </div>
+                                <?php
+                                $tong = 0;
+                                $i = 0;
+                                foreach ($_SESSION['mycart'] as $cart) {
+                                    $ttien = $cart[3] * $cart[4];
+                                    $tong += $ttien;
+                                    $xoasp = '<a href="index.php?act=deletecart&idcart=' . $i . '" ><input type="button" value="Xóa"></a>';
+                                    echo '    
+                                    <div class="your-order-middle">
+                                <ul>
+                                    <li><span class="order-middle-left">' . $cart[1] . '</span> <span class="order-price">' . $cart[3] . '</span></li>
+                                </ul>
+                                </div>  
+                                ';
+                                    $i += 1;
+                                }
+                                ?>
+
                                 <div class="your-order-bottom">
                                     <ul>
                                         <li class="your-order-shipping">Shipping</li>
@@ -55,7 +67,7 @@
                                 <div class="your-order-total">
                                     <ul>
                                         <li class="order-total">Total</li>
-                                        <li>$329</li>
+                                        <li><span name="tongtien"><?= $tong ?></span></li>
                                     </ul>
                                 </div>
                             </div>
@@ -101,10 +113,17 @@
                             </div>
                         </div>
                         <div class="Place-order mt-25">
-                            <a class="btn-hover" href="#">Place Order</a>
+                            <!-- <a class="btn-hover" href="index.php?act=donhang" name="thanhtoan">Place Order</a> -->
+                            <input type="submit" class="btn-hover" name="thanhtoan" value="Thanh toán"><br>
                         </div>
+                        <?php
+                        if (isset($thongbao) && ($thongbao != "")) {
+                            echo $thongbao;
+                        }
+                        ?>
                     </div>
                 </div>
             </div>
-        </div>
+        </form>
     </div>
+</div>
